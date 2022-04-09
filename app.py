731863f85ef53c5382af79cd8d1d90574a9d1add
Ajeet Shukla <98910114+ajeetshukla94@users.noreply.github.com>
2022-04-10 02:00:24 +0530

    132

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import nltk
 import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app = dash.Dash(__name__)
 server = app.server
